58a13039af828b78908417b099410142569ecac9
itsmocomfort <79454683+itsmocomfort@users.noreply.github.com>
2021-06-30 10:35:18 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Counter from './Counter'
+import Header from './Header';
 
 const App = () => {
     return (
         <div style={{'margin':"200px auto",'display':'flex',"flexDirection":"column","alignItems":"center"}}>
-            <h1>김재천</h1>
+            <Header />
             <Counter />
             <Counter num="10" />
             <h2><a href="https://github.com/itsmocomfort/front_End">https://github.com/itsmocomfort/front_End</a></h2>
